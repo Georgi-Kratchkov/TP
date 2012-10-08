fc69ca264ae4d037b9cc1b9cce125b9ade3d421c
Georgi Kratchkov <georgi_kratchkov@abv.bg>
2012-10-08 21:27:05 +0300

    Homework 3

---
+++ h3/1.rb
@@ -0,0 +1,17 @@
+require "csv"
+
+def process_file_with_csv	
+	result1 = 0
+	result2 = 0
+	CSV.foreach("List.csv") do |row|
+	if row[4] == " Ruby"
+		result1 = result1 + row[2].to_i	
+		else 
+		result2 = result2 + row[2].to_i	
+		end
+	end
+	p result1
+	p result2
+
+end
+process_file_with_csv
