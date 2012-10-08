135492eeeceae454c48f54ee296380e71ed6da84
Georgi Kratchkov <georgi_kratchkov@abv.bg>
2012-10-08 21:26:46 +0300

    Homework 2

---
+++ h2/1.rb
@@ -0,0 +1,28 @@
+=begin
+					ТУЕС (Технологично училище Електронни системи към ТУ София)
+										http://www.elsys-bg.org/
+=end									
+
+
+require 'csv'
+
+def process_file_with_csv	
+	result = 0
+	CSV.foreach("list1.csv") do |row|
+		result = result + row[3].to_i	
+	end
+	p result
+
+end
+def process_file
+	f = File.open("list1.csv", "r")
+	content = f.read
+	result = 0
+	content.each_line do |line|
+		array = line.split(",")
+		result = result + array[3].to_i
+	end
+	p result
+end
+
+process_file_with_csv
